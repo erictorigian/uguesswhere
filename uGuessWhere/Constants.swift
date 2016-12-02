52d97725071d66dc431166091e34daa92f5b2348
erictorigian <eric@torigian.com>
2016-12-02 13:07:29 -0500

    added login UI

---
+++ uGuessWhere/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  iWasHere
+//
+//  Created by Eric Torigian on 10/27/16.
+//  Copyright © 2016 Eric Torigian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
+
+//Keys
+let KEY_UID = "uid"
+
+//Cells
+let KEY_SAVED_LOC_CELL = "savedPlacesCell"
+let KEY_AVAILABLE_GAMES_CELL = "gamesCell"
+
+
+//Segues
+let SEGUE_LOGGED_IN = "loggedIn"
+let SEGUE_SHOW_LANDMARK_DETAIL = "showLandmarkDetail"
+
+
+//Status codes
+let STATUS_ACCOUNT_NONEXISTS = 17011
+let STATUS_INVALID_PWD = 17009
+let STATUS_INVALID_EMAIL = 17999
+let STATUS_ACCOUNT_DISABLED = 17005

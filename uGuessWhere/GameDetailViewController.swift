b1a7ec287d66e3c1ef7a12f2d320db72b92aee2e
Eric Torigian <eric@erics-air.hsd1.mi.comcast.net>
2016-12-28 21:49:26 -0500

    added new game UI

--- uGuessWhere/GameDetailViewController.swift
+++ uGuessWhere/GameDetailViewController.swift
@@ -13,12 +13,34 @@ class GameDetailViewController: UIViewController {
     var game: Game!
     
     @IBOutlet weak var gameNameLabel: UINavigationItem!
+    @IBOutlet weak var gameOwnerLabel: UILabel!
+    @IBOutlet weak var gameStartTimeLabel: UILabel!
+    @IBOutlet weak var numberOfGuessesLabel: UILabel!
+    @IBOutlet weak var gameStatusLabel: UILabel!
+    @IBOutlet weak var difficultyLevelLabel: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("detail: \(game)")
         gameNameLabel.title = game.gameName
+        
+        gameOwnerLabel.text = game.gameOwner
+        gameStartTimeLabel.text = game.gameStartTime
+        gameStatusLabel.text = game.gameStatus
+        numberOfGuessesLabel.text = String(game.numberOfGuesses)
+        
+        switch game.numberOfGuesses {
+        case 0..<10:
+            difficultyLevelLabel.text = "Easy"
+            difficultyLevelLabel.textColor = UIColor.green
+        case 11..<20:
+            difficultyLevelLabel.text = "Medium"
+            difficultyLevelLabel.textColor = UIColor.yellow
+        default:
+            difficultyLevelLabel.text = "Hard"
+            difficultyLevelLabel.textColor = UIColor.red
+         }
     }
 
 }
---
+++ uGuessWhere/NewGameViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewGameViewController.swift
+//  uGuessWhere
+//
+//  Created by Eric Torigian on 12/28/16.
+//  Copyright © 2016 Eric Torigian. All rights reserved.
+//
+
+import UIKit
+
+class NewGameViewController: UIViewController {
+
+    var game: Game!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
